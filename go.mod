9c022224e2ed571cdaad02492a8d5feded60fd77
Jiacheng Xu <xjcmaxwellcjx@gmail.com>
2019-11-06 22:11:16 +0800

    finish the deletion handle part

--- go.mod
+++ go.mod
@@ -7,8 +7,10 @@ require (
 	github.com/go-logr/logr v0.1.0
 	github.com/google/go-cmp v0.3.0
 	github.com/spf13/cobra v0.0.5
+	github.com/stretchr/testify v1.3.0
 	golang.org/x/crypto v0.0.0-20190611184440-5c40567a22f8 // indirect
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45 // indirect
+	k8s.io/api v0.0.0-20190918195907-bd6ac527cfd2
 	k8s.io/apimachinery v0.0.0-20190817020851-f2f3a405f61d
 	k8s.io/client-go v0.0.0-20190918200256-06eb1244587a
 	k8s.io/utils v0.0.0-20190801114015-581e00157fb1 // indirect
--- pkg/manager/internal/controller/tenant_controller.go
+++ pkg/manager/internal/controller/tenant_controller.go
@@ -21,11 +21,18 @@ import (
 	"fmt"
 
 	"github.com/go-logr/logr"
+	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	catalogv1alpha1 "github.com/kubermatic/kubecarrier/pkg/apis/catalog/v1alpha1"
+	"github.com/kubermatic/kubecarrier/pkg/manager/internal/util"
+)
+
+const (
+	tenantControllerFinalizer util.Finalizer = "tenant.kubecarrier.io/controller"
 )
 
 // TenantReconciler reconciles a Tenant object
@@ -37,14 +44,33 @@ type TenantReconciler struct {
 
 // +kubebuilder:rbac:groups=catalog.kubecarrier.io,resources=tenants,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=catalog.kubecarrier.io,resources=tenants/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 
+// Reconcile function reconciles the Tenant object which specified by the request. Currently, it does the following:
+// 1. Fetch the Tenant object.
+// 2. Handle the deletion of the Tenant object (Remove the namespace that the tenant owns, and remove the finalizer).
+// 3. Handle the creation/update of the Tenant object (Create/reconcile the namespace and insert the finalizer).
+// 4. Update the status of the Tenant object.
 func (r *TenantReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("tenant", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("tenant", req.NamespacedName)
 
-	fmt.Println("I am running")
+	// 1. Fetch the Tenant object.
+	tenant := &catalogv1alpha1.Tenant{}
+	if err := r.Get(ctx, req.NamespacedName, tenant); err != nil {
+		// If the Tenant object is already gone, we just ignore the NotFound error.
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
-	// your logic here
+	// 2. Handle the deletion of the Tenant object.
+	if !tenant.DeletionTimestamp.IsZero() {
+		if err := r.handleDeletion(ctx, log, tenant); err != nil {
+			return ctrl.Result{}, fmt.Errorf("handling deletion: %w", err)
+		}
+		return ctrl.Result{}, nil
+	}
+
+	// 3. reconcile the Tenant object.
 
 	return ctrl.Result{}, nil
 }
@@ -53,4 +79,41 @@ func (r *TenantReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&catalogv1alpha1.Tenant{}).
 		Complete(r)
+}
+
+// handleDeletion handles the deletion of the Tenant object. Currently, it does:
+// 1. Delete the Namespace that the tenant owns.
+// 2. Remove the finalizer from the tenant object.
+func (r *TenantReconciler) handleDeletion(ctx context.Context, log logr.Logger, tenant *catalogv1alpha1.Tenant) error {
+
+	// 1. Delete the Namespace.
+	if err := r.deleteNamespace(ctx, log, tenant); err != nil {
+		return err
+	}
+
+	// 2. The Namespace is completely removed, then we remove the finalizer here.
+	if tenantControllerFinalizer.Delete(tenant) {
+		if err := r.Update(ctx, tenant); err != nil {
+			return fmt.Errorf("updating Tenant Status: %w", err)
+		}
+	}
+	return nil
+}
+
+// deleteNamespace deletes the Namespace completely from the cluster, if it is not completely removed, an error will be returned.
+func (r *TenantReconciler) deleteNamespace(ctx context.Context, log logr.Logger, tenant *catalogv1alpha1.Tenant) error {
+	ns := &corev1.Namespace{}
+	if err := r.Get(ctx, types.NamespacedName{Name: tenant.Status.NamespaceName}, ns); err != nil {
+		// If the namespace is already gone, then we don't return error.
+		return client.IgnoreNotFound(err)
+	}
+
+	// If the namespace is found, then it need to be deleted.
+	if err := r.Delete(ctx, ns); err != nil {
+		return fmt.Errorf("deleting Namespace: %w", err)
+	}
+
+	// We want to make sure the namespace is completely deleted, then we can remove the finalizer, so here we return an error
+	// to move to the next reconcile round.
+	return fmt.Errorf("deleting Namespace: waiting the namespace to be removed/shutdown completely")
 }
