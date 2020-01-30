f4af4366edd7e7edc86f7ea64009311918b414cb
Neven Miculinic <neven.miculinic@gmail.com>
2020-01-30 14:45:10 +0100

    Ferry ServiceClusterAssignment assignment (#136)
    * Added tender controller & serviceclusterregistration object
    
    * Updated the tender role & added serviceClusterRegistration to the operator
    
    * moved serviceclusterregistration ControllerObjects closer to their usage
    
    * Added cmd logging mixin
    
    * Added tender cmd log mixing
    
    * update
    
    * Renamed tender to ferry
    
    * small nit
    
    * minor update
    
    * updated inline with master
    
    * Added service cluster controller
    
    * fixed e2e test and made sure it's working
    
    * renamed components
    
    * Refactored and simplified ferry main.go
    
    * fixed merging
    
    * Fixed rest of forgotten merge conflicts screwing up the e2e tests
    
    * lint
    
    * Initial CRDDiscovery implementation
    
    Currently e2e tests are missing
    
    * Some initial e2e test implementation. Heavy WIP
    
    * WIP --> amend this commit
    
    * merged origin/master
    
    * Renamed CRDDiscovery to CustromResourceDefinitionDiscovery
    
    * added disabled pruning for crd
    
    * made unit tests passing
    
    * Fixed CustomResouceDefinitionDiscovery
    
    * Added env configuration for gen-tasks.go
    
    * WIP --> tenantAssignment
    
    * [WIP] -- dunno really; it's left since I started working
    
    * refactored predicate to predicateFn
    
    * Removed delete CRD permissino from ferry on the CRDDiscovery controller
    
    * Better document servicecluster RBAC issues
    
    * update
    
    * renamed all crdReference to crddiscovery
    
    * Update hack/codegen.sh
    
    Co-Authored-By: Jiacheng Xu <xjcmaxwellcjx@gmail.com>
    
    * replaced ErrorReason with CRDNotFound
    
    * regen statiks
    
    * perform cleanup after the ferry tests are run
    
    * fix stuff
    
    * fixed (undetected) merge configs
    
    * added some better test error messages
    
    * fixing deleting functionality
    
    * Fixed possible namespace object leak
    
    The namespace leaks happens in following circumstances:
    * Namespace has been created for serviceclusterassignment
    * Controller crash before setting the NamespaceName in the status
    * The user deletes the ServiceClusterAssignment
    * The object gets deleted, leaving a namespace object leak
    
    This commit fixes it.
    
    * fix minor test error
    
    * Update pkg/ferry/internal/controllers/serviceclusterassignment_controller.go
    
    Co-Authored-By: Nico Schieder <nico@loodse.com>
    
    * Removed tenant mention from service cluster assignment
    
    * Removed namespace role within the master cluster
    
    * refactored UpsertNamespace to EnsureUniqueNamespace
    
    * removed ListOwnedNamespaced function
    
    Co-authored-by: Jiacheng Xu <xjcmaxwellcjx@gmail.com>
    Co-authored-by: Nico Schieder <Nico.schieder@gmail.com>

---
+++ pkg/ferry/internal/controllers/serviceclusterassignment_controller_test.go
@@ -0,0 +1,68 @@
+/*
+Copyright 2019 The KubeCarrier Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controllers
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	ctrl "sigs.k8s.io/controller-runtime"
+	fakeclient "sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	corev1alpha1 "github.com/kubermatic/kubecarrier/pkg/apis/core/v1alpha1"
+	"github.com/kubermatic/kubecarrier/pkg/testutil"
+)
+
+func TestServiceClusterAssignmentReconciler(t *testing.T) {
+	serviceClusterAssignment := &corev1alpha1.ServiceClusterAssignment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo.eu-west-1",
+			Namespace: "provider-bar",
+		},
+		Spec: corev1alpha1.ServiceClusterAssignmentSpec{
+			ServiceCluster: corev1alpha1.ObjectReference{
+				Name: "eu-west-1",
+			},
+		},
+	}
+
+	r := ServiceClusterAssignmentReconciler{
+		Log:           testutil.NewLogger(t),
+		MasterClient:  fakeclient.NewFakeClientWithScheme(testScheme, serviceClusterAssignment),
+		MasterScheme:  testScheme,
+		ServiceClient: fakeclient.NewFakeClientWithScheme(testScheme),
+	}
+	_, err := r.Reconcile(ctrl.Request{
+		NamespacedName: types.NamespacedName{
+			Name:      serviceClusterAssignment.Name,
+			Namespace: serviceClusterAssignment.Namespace,
+		},
+	})
+	require.NoError(t, err, "Reconcile")
+
+	ctx := context.Background()
+	namespaceList := &corev1.NamespaceList{}
+	require.NoError(t, r.ServiceClient.List(ctx, namespaceList), "listing Namespaces")
+	if assert.Len(t, namespaceList.Items, 1) {
+		assert.Equal(t, "serviceclusterassignment-", namespaceList.Items[0].GenerateName)
+	}
+}
