33c1692c8ad7d3de971e05a86d4d3d0d8ddb6b8a
Neven Miculinic <neven.miculinic@gmail.com>
2020-02-10 08:56:41 +0100

    Added long SUT description

--- pkg/anchor/internal/cmd/sut/sut.go
+++ pkg/anchor/internal/cmd/sut/sut.go
@@ -25,9 +25,35 @@ import (
 
 func NewCommand(log logr.Logger) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "sut",
-		Long:  strings.TrimSpace(``),
-		Short: "sut testing utilities",
+		Use: "sut",
+		Long: strings.TrimSpace(`
+SUT is the primary kubecarrier debugging utility.
+
+Upon running it "replaces" a running deployment with telepresence and prepares the necessary IDE tasks for its proper running with approximately similar config (mounts are remapped due to different mount points, i.e. containers root isn't your host's root). Currently the following components should work:
+
+* [x] catapult
+* [x] ferry
+* [x] eleveator
+* [x] manager
+* [x] operator
+
+and the following features are implemented:
+
+* [x] RBAC --> running SUT tasks use the kubernetes service account credentials which our component has in the cluster
+* [x] mount rebinding --> SUT tasks should have its args/env filepath remapped to different root mount point
+* [x] webhook support --> the right ports should be exposed, thus when running SUT task the webhooks reaches the local process
+* [x] component auto-discovery; a fzf based picker shows which component exists for faster CLI usage
+
+Currently not implemented:
+* [ ] kube DNS & clusterIP routing --> if the host's process tries using in-cluster DNS/clusterIPs this probably won't work for now.
+
+Examples:
+
+anchor sut manager
+
+the tool shall pause and print the telepresence command you ought to run in separate terminal. After the command reaches running state press any key to continue. The SUT utility shall generate necessary InteliJ/vscode configuration ready for this component.
+`),
+		Short: "SUT connects to running cluster, replaces target kubecarrier's component with telepresence, and configures IDE task setup for debugging purposes",
 	}
 
 	cmd.AddCommand(
