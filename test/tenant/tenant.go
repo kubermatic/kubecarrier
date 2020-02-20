91e092dda0806631a223cae9e54e2ed9060e4038
Neven Miculinic <neven.miculinic@gmail.com>
2020-02-20 11:38:51 +0100

    Modernized and fixed tenant test

--- test/tenant/tenant.go
+++ test/tenant/tenant.go
@@ -18,184 +18,88 @@ package tenant
 
 import (
 	"context"
-	"fmt"
-	"time"
+	"testing"
 
-	"github.com/stretchr/testify/suite"
-	corev1 "k8s.io/api/core/v1"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/util/wait"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	catalogv1alpha1 "github.com/kubermatic/kubecarrier/pkg/apis/catalog/v1alpha1"
+	"github.com/kubermatic/kubecarrier/pkg/testutil"
 	"github.com/kubermatic/kubecarrier/test/framework"
 )
 
-var (
-	_ suite.SetupAllSuite    = (*TenantSuite)(nil)
-	_ suite.TearDownAllSuite = (*TenantSuite)(nil)
-)
-
-// TenantSuite verify Tenant related operations (CatalogEntries, creating service instances).
-type TenantSuite struct {
-	suite.Suite
-	*framework.Framework
-
-	masterClient  client.Client
-	serviceClient client.Client
-
-	// Objects that used in this test suite.
-	provider *catalogv1alpha1.Account
-	crd      *apiextensionsv1.CustomResourceDefinition
-}
-
-func (s *TenantSuite) SetupSuite() {
-	var err error
-	ctx := context.Background()
-	s.masterClient, err = s.MasterClient()
-	s.Require().NoError(err, "creating master client")
-
-	s.serviceClient, err = s.ServiceClient()
-	s.Require().NoError(err, "creating service client")
-
-	// Create a Provider to execute tests
-	s.provider = &catalogv1alpha1.Account{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "other-cloud",
-		},
-		Spec: catalogv1alpha1.AccountSpec{
-			Metadata: catalogv1alpha1.AccountMetadata{
-				DisplayName: "provider1",
-				Description: "provider1 test description",
-			},
-			Roles: []catalogv1alpha1.AccountRole{
-				catalogv1alpha1.ProviderRole,
-			},
-		},
-	}
-	s.Require().NoError(s.masterClient.Create(ctx, s.provider), "creating provider error")
-
-	// Try to get the namespace that created for this provider.
-	namespace := &corev1.Namespace{}
-	s.NoError(wait.Poll(time.Second, 10*time.Second, func() (done bool, err error) {
-		if err := s.masterClient.Get(ctx, types.NamespacedName{
-			Name: fmt.Sprintf("provider-%s", s.provider.Name),
-		}, namespace); err != nil {
-			if errors.IsNotFound(err) {
-				return false, nil
-			}
-			return true, err
-
-		}
-		return true, nil
-	}), "getting the namespace for the Provider error")
-
-	// Create CRDs to execute tests
-	s.crd = &apiextensionsv1.CustomResourceDefinition{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "couchdbs.eu-west-1.example.cloud",
-			Labels: map[string]string{
-				"kubecarrier.io/provider":        s.provider.Name,
-				"kubecarrier.io/service-cluster": "eu-west-1",
-			},
-		},
-		Spec: apiextensionsv1.CustomResourceDefinitionSpec{
-			Group: "eu-west-1.example.cloud",
-			Names: apiextensionsv1.CustomResourceDefinitionNames{
-				Plural: "couchdbs",
-				Kind:   "CouchDB",
-			},
-			Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
-				{
-					Name:    "v1alpha1",
-					Storage: true,
-					Schema: &apiextensionsv1.CustomResourceValidation{
-						OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
-							Type: "object",
-						},
+func NewTenantSuite(f *framework.Framework) func(t *testing.T) {
+	return func(t *testing.T) {
+		masterClient, err := f.MasterClient()
+		require.NoError(t, err, "creating master client")
+		defer masterClient.CleanUp(t)
+		ctx := context.Background()
+		var (
+			provider = &catalogv1alpha1.Account{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "other-cloud",
+				},
+				Spec: catalogv1alpha1.AccountSpec{
+					Metadata: catalogv1alpha1.AccountMetadata{
+						DisplayName: "provider1",
+						Description: "provider1 test description",
+					},
+					Roles: []catalogv1alpha1.AccountRole{
+						catalogv1alpha1.ProviderRole,
 					},
 				},
+			}
+			crd = &apiextensionsv1.CustomResourceDefinition{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "couchdbs.eu-east-1.example.cloud",
+					Labels: map[string]string{
+						"kubecarrier.io/provider":        provider.Name,
+						"kubecarrier.io/service-cluster": "eu-east-1",
+					},
+				},
+				Spec: apiextensionsv1.CustomResourceDefinitionSpec{
+					Group: "eu-east-1.example.cloud",
+					Names: apiextensionsv1.CustomResourceDefinitionNames{
+						Plural: "couchdbs",
+						Kind:   "CouchDB",
+					},
+					Versions: []apiextensionsv1.CustomResourceDefinitionVersion{
+						{
+							Name:    "v1alpha1",
+							Storage: true,
+							Schema: &apiextensionsv1.CustomResourceValidation{
+								OpenAPIV3Schema: &apiextensionsv1.JSONSchemaProps{
+									Type: "object",
+								},
+							},
+						},
+					},
+					Scope: apiextensionsv1.NamespaceScoped,
+				},
+			}
+		)
+		require.NoError(t, masterClient.Create(ctx, provider))
+		require.NoError(t, masterClient.Create(ctx, crd))
+		require.NoError(t, testutil.WaitUntilReady(masterClient, provider))
+
+		catalogEntry := &catalogv1alpha1.CatalogEntry{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "couchdbs",
+				Namespace: provider.Status.NamespaceName,
 			},
-			Scope: apiextensionsv1.NamespaceScoped,
-		},
+			Spec: catalogv1alpha1.CatalogEntrySpec{
+				Metadata: catalogv1alpha1.CatalogEntryMetadata{
+					DisplayName: "Couch DB",
+					Description: "The comfy nosql database",
+				},
+				ReferencedCRD: catalogv1alpha1.ObjectReference{
+					Name: crd.Name,
+				},
+			},
+		}
+		require.NoError(t, masterClient.Create(ctx, catalogEntry), "creating catalogEntry error")
+		assert.NoError(t, testutil.WaitUntilReady(masterClient, catalogEntry), "catalog entry not ready within time limit")
 	}
-
-	s.Require().NoError(s.masterClient.Create(ctx, s.crd), fmt.Sprintf("creating CRD: %s error", s.crd.Name))
-
-}
-
-func (s *TenantSuite) TestCatalogEntryCreationAndDeletion() {
-	ctx := context.Background()
-	catalogEntry := &catalogv1alpha1.CatalogEntry{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "couchdbs",
-			Namespace: fmt.Sprintf("provider-%s", s.provider.Name),
-		},
-		Spec: catalogv1alpha1.CatalogEntrySpec{
-			Metadata: catalogv1alpha1.CatalogEntryMetadata{
-				DisplayName: "Couch DB",
-				Description: "The comfy nosql database",
-			},
-			ReferencedCRD: catalogv1alpha1.ObjectReference{
-				Name: s.crd.Name,
-			},
-		},
-	}
-
-	// Create the CatalogEntry
-	s.Require().NoError(s.masterClient.Create(ctx, catalogEntry), "creating catalogEntry error")
-
-	// Check the status of the CatalogEntry.
-	catalogEntryFound := &catalogv1alpha1.CatalogEntry{}
-	s.NoError(wait.Poll(time.Second, 10*time.Second, func() (done bool, err error) {
-		if err := s.masterClient.Get(ctx, types.NamespacedName{
-			Name:      catalogEntry.Name,
-			Namespace: catalogEntry.Namespace,
-		}, catalogEntryFound); err != nil {
-			if errors.IsNotFound(err) {
-				return false, nil
-			}
-			return true, err
-		}
-		return catalogEntryFound.Status.CRD.Name == s.crd.Name, nil
-	}), "getting the CatalogEntry error")
-
-	// Delete the CatalogEntry
-	s.Require().NoError(wait.Poll(time.Second, 10*time.Second, func() (done bool, err error) {
-		if err = s.masterClient.Delete(ctx, catalogEntry); err != nil {
-			if errors.IsNotFound(err) {
-				return true, nil
-			}
-			return false, err
-		}
-		return false, nil
-	}), "deleting the CatalogEntry error")
-}
-
-func (s *TenantSuite) TearDownSuite() {
-	ctx := context.Background()
-	// Remove the CRDs for testing.
-	s.Require().NoError(wait.Poll(time.Second, 10*time.Second, func() (done bool, err error) {
-		if err = s.masterClient.Delete(ctx, s.crd); err != nil {
-			if errors.IsNotFound(err) {
-				return true, nil
-			}
-			return false, err
-		}
-		return false, nil
-	}), fmt.Sprintf("deleting CRD: %s error", s.crd.Name))
-
-	// Remove the provider for testing.
-	s.Require().NoError(wait.Poll(time.Second, 30*time.Second, func() (done bool, err error) {
-		if err = s.masterClient.Delete(ctx, s.provider); err != nil {
-			if errors.IsNotFound(err) {
-				return true, nil
-			}
-			return false, err
-		}
-		return false, nil
-	}), "could not delete the Provider")
 }
