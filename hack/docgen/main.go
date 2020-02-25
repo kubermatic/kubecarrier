42698f1890cd45ce2b890c9695cf90abeac514c1
Neven Miculinic <neven.miculinic@gmail.com>
2020-02-25 14:47:09 +0100

    Added sorting to the doc generation

--- hack/docgen/main.go
+++ hack/docgen/main.go
@@ -29,6 +29,7 @@ import (
 	"os"
 	"path"
 	"reflect"
+	"sort"
 	"strings"
 )
 
@@ -277,6 +278,7 @@ func main() {
 	if args[0] == "--" {
 		args = args[1:]
 	}
-	_, _ = fmt.Fprint(os.Stderr, args)
+	sort.Sort(sort.StringSlice(args))
+	_, _ = fmt.Fprint(os.Stderr, len(args), args)
 	printAPIDocs(args)
 }
