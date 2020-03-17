824ea265ad3c1f856cbc8ed5b1e368a491263df7
Neven Miculinic <neven.miculinic@gmail.com>
2020-03-17 19:03:33 +0100

    Added account deletion helper

---
+++ pkg/cli/internal/cmd/delete/account.go
@@ -0,0 +1,113 @@
+/*
+Copyright 2019 The KubeCarrier Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package deletecmd
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/go-logr/logr"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	catalogv1alpha1 "github.com/kubermatic/kubecarrier/pkg/apis/catalog/v1alpha1"
+	corev1alpha1 "github.com/kubermatic/kubecarrier/pkg/apis/core/v1alpha1"
+	"github.com/kubermatic/kubecarrier/pkg/internal/util"
+)
+
+func newAccountCommand(log logr.Logger, cl *util.ClientWatcher) *cobra.Command {
+	var (
+		force bool
+		all   bool
+	)
+	cmd := &cobra.Command{
+		Args:  cobra.MaximumNArgs(1),
+		Use:   "account",
+		Short: "account NAME [--all] [--force]",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if all && len(args) > 0 {
+				return fmt.Errorf("either --all or specific account must be specified")
+			}
+			if !all && len(args) == 0 {
+				return fmt.Errorf("one of --all or specific account must be specified")
+			}
+
+			ctx := context.Background()
+
+			if all {
+				list := &catalogv1alpha1.AccountList{}
+				if err := cl.List(ctx, list); err != nil {
+					return err
+				}
+				for _, it := range list.Items {
+					args = append(args, it.Name)
+				}
+			}
+			for _, it := range args {
+				fmt.Println("deleting account", "name", it)
+				account := &catalogv1alpha1.Account{
+					ObjectMeta: metav1.ObjectMeta{Name: it},
+				}
+				err := cl.Delete(ctx, account)
+				if err != nil {
+					if !force || !strings.HasPrefix(err.Error(), "admission webhook \"vaccount.kubecarrier.io\" denied the request: deletion blocking objects found") {
+						return err
+					}
+
+					for _, objKind := range []runtime.Object{
+						&catalogv1alpha1.CatalogEntrySetList{},
+						&catalogv1alpha1.CatalogEntryList{},
+						&catalogv1alpha1.DerivedCustomResourceList{},
+						&corev1alpha1.CustomResourceDiscoverySetList{},
+						&corev1alpha1.CustomResourceDiscoveryList{},
+						&corev1alpha1.ServiceClusterAssignmentList{},
+					} {
+						if err := cl.List(ctx, objKind, client.InNamespace(account.Status.Namespace.Name)); err != nil {
+							return err
+						}
+						objs, err := meta.ExtractList(objKind)
+						if err != nil {
+							return err
+						}
+						for _, obj := range objs {
+							fmt.Println("deleting", util.MustLogLine(obj, scheme))
+							if err := cl.Delete(ctx, obj); err != nil {
+								return err
+							}
+						}
+						for _, obj := range objs {
+							if err := cl.WaitUntilNotFound(ctx, obj); err != nil {
+								return err
+							}
+						}
+					}
+				}
+				if err := cl.WaitUntilNotFound(ctx, account); err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+	}
+	cmd.Flags().BoolVar(&force, "force", false, "force delete the account")
+	cmd.Flags().BoolVar(&all, "all", false, "delete all accounts")
+	return cmd
+}
