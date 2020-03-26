967291f920a51ca3e05148235775fabe392af968
Neven Miculinic <neven.miculinic@gmail.com>
2020-03-26 10:32:01 +0100

    fixed linter errors

--- pkg/manager/internal/webhooks/customresourcediscovery_webhook.go
+++ pkg/manager/internal/webhooks/customresourcediscovery_webhook.go
@@ -20,20 +20,14 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/go-logr/logr"
 	adminv1beta1 "k8s.io/api/admission/v1beta1"
-	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
-	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/webhook/admission"
 
 	corev1alpha1 "github.com/kubermatic/kubecarrier/pkg/apis/core/v1alpha1"
-	"github.com/kubermatic/kubecarrier/pkg/internal/util"
 )
 
 // CustomResourceDiscoveryWebhookHandler handles mutating/validating of CustomResourceDiscoveries.
@@ -67,13 +61,6 @@ func (r *CustomResourceDiscoveryWebhookHandler) Handle(ctx context.Context, req 
 	case adminv1beta1.Delete:
 		// TODO: FIX THIS!
 		return admission.Allowed("temporary disabled due to bug")
-		oldObj := &corev1alpha1.CustomResourceDiscovery{}
-		if err := r.decoder.DecodeRaw(req.OldObject, oldObj); err != nil {
-			return admission.Errored(http.StatusBadRequest, err)
-		}
-		if err := r.validateDelete(ctx, oldObj); err != nil {
-			return admission.Denied(err.Error())
-		}
 	}
 	return admission.Allowed("allowed to commit the request")
 
@@ -96,37 +83,4 @@ func (r *CustomResourceDiscoveryWebhookHandler) validateUpdate(oldObj, newObj *c
 		return fmt.Errorf("the Spec (ServiceCluster, CRD, and KindOverride) of CustomResourceDiscovery is immutable")
 	}
 	return nil
-}
-
-func (r *CustomResourceDiscoveryWebhookHandler) validateDelete(ctx context.Context, obj *corev1alpha1.CustomResourceDiscovery) error {
-	r.Log.Info("validate delete", "name", obj.Name)
-	if obj.Status.ManagementClusterCRD == nil {
-		return nil
-	}
-
-	crd := &apiextensionsv1.CustomResourceDefinition{}
-	if err := r.Get(ctx, types.NamespacedName{
-		Name: obj.Status.ManagementClusterCRD.Name,
-	}, crd); err != nil {
-		return err
-	}
-	u := &unstructured.UnstructuredList{}
-	u.SetGroupVersionKind(schema.GroupVersionKind{
-		Group:   crd.Spec.Group,
-		Version: crd.Spec.Versions[0].Name,
-		Kind:    crd.Spec.Names.ListKind,
-	})
-	if err := r.List(ctx, u); err != nil {
-		return err
-	}
-	if len(u.Items) == 0 {
-		return nil
-	}
-
-	errorMsg := new(strings.Builder)
-	errorMsg.WriteString("management cluster CRD instances are still present in the management cluster\n")
-	for _, it := range u.Items {
-		errorMsg.WriteString(util.MustLogLine(&it, r.Scheme) + " still present\n")
-	}
-	return fmt.Errorf("%s", errorMsg)
 }
