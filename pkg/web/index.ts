179799a15aba6f1cb8b1e4fa098f0708a034e24d
Neven Miculinic <neven.miculinic@gmail.com>
2020-04-15 16:34:06 +0200

    update

--- pkg/web/index.ts
+++ pkg/web/index.ts
@@ -1,6 +1,3 @@
-// import { APIVersion, VersionRequest } from "../apis/apiserver/v1alpha1/kubecarrier_pb";
-// import {KubecarrierClient} from "../apis/apiserver/v1alpha1/KubecarrierServiceClientPb";
-
 import {VersionRequest } from "./kubecarrier_pb";
 import {KubecarrierClient } from "./KubecarrierServiceClientPb";
 
@@ -8,9 +5,9 @@ console.log("I'm alive and working...more  or less")
 let request = new VersionRequest()
 let client = new KubecarrierClient("http://" + window.location.hostname + ":8090", null, null)
 client.version(request, {}, ((err, response) => {
-    console.log("get message back v3")
     if (err) {
         console.error(err.code, err.message)
+    } else {
+        console.log(`version: ${response.getVersion()}`)
     }
-    console.log(`version: ${response.getVersion()}`)
 }));
