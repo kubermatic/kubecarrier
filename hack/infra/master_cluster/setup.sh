c3255df1c150a947e3e52753794ae3fa9636809e
Neven Miculinic <neven.miculinic@gmail.com>
2020-04-23 16:29:54 +0200

    Some updates

--- hack/infra/master_cluster/setup.sh
+++ hack/infra/master_cluster/setup.sh
@@ -27,17 +27,28 @@ echo "export VAULT_ADDR=${VAULT_ADDR}"
 export AWS_DEFAULT_REGION=eu-central-1
 
 # shellcheck disable=SC1090
-source <(vault kv get -field=aws_dns_user dev/kubecarrier)
+source <(vault kv get -field=aws_poweruser dev/kubecarrier)
+#source <(vault kv get -field=aws_dns_user dev/kubecarrier)
 
+AWS_CREDENTIALS=$(
+  cat <<EOF
+[default]
+aws_access_key_id = ${AWS_ACCESS_KEY_ID}
+aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
+EOF
+)
+
+echo "=== creating required secrets ==="
+kubectl create secret generic -n cert-manager route53-aws "--from-literal=id=${AWS_ACCESS_KEY_ID}" "--from-literal=secret=${AWS_SECRET_ACCESS_KEY}" --dry-run=client -o yaml | kubectl apply -f -
+
+echo "=== installing cert-manager ==="
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v0.14.0/cert-manager.yaml
-helm upgrade dex stable/dex  --install  --values ./dex_values.yaml
-
 kubectl wait --for=condition=available deployment/cert-manager -n cert-manager --timeout=120s
 kubectl wait --for=condition=available deployment/cert-manager-cainjector -n cert-manager --timeout=120s
 kubectl wait --for=condition=available deployment/cert-manager-webhook -n cert-manager --timeout=120s
 
-kubectl secret genetic route53-aws "--from-literal=id=${AWS_ACCESS_KEY_ID}" "--from-literal=secret=${AWS_SECRET_ACCESS_KEY}" --dry-run -o yaml | kubectl apply -f -
-cat << EOF | kubectl apply -f -
+echo "=== creating ClusterIssuer ==="
+cat <<EOF | kubectl apply -f -
 apiVersion: cert-manager.io/v1alpha3
 kind: ClusterIssuer
 metadata:
@@ -53,16 +64,20 @@ spec:
         dnsZones:
         - lab.kubecarrier.io
       dns01:
-        providers:
-        - name: route53
-          route53:
-            region: us-central-1
-            accessKeyID: "${AWS_ACCESS_KEY_ID}"
-            secretAccessKeySecretRef:
-              name:
-                name: route53-aws
-                key: secret
-            hostedZoneID: Z04311331EAB7S9MUWBEW
+        route53:
+          region: us-central-1
+          accessKeyID: "${AWS_ACCESS_KEY_ID}"
+          secretAccessKeySecretRef:
+            name: route53-aws
+            key: secret
+          hostedZoneID: Z04311331EAB7S9MUWBEW
 EOF
 
+echo "=== installing dex ==="
+helm upgrade dex stable/dex --install --values ./dex_values.yaml
 kubectl apply -f dex_ingress.yaml
+
+echo "=== installing external -dns ==="
+
+kubectl create secret generic external-dns-route53-aws "--from-literal=credentials=${AWS_CREDENTIALS}" --dry-run=client -o yaml | kubectl apply -f -
+helm upgrade external-dns bitnami/external-dns --install --values ./external-dns_values.yaml
